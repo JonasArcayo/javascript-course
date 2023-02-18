33a52ed0033bf17b559f03f72b0028933d863247
Jonas <jonasarcayo@gmail.com>
2023-02-18 01:21:05 -0500

    feat: Adding navar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAV8Xzrwm85I8gNrIk2UX27q-NS2EcgsP4",
+  authDomain: "netflix-clone-ec7dc.firebaseapp.com",
+  projectId: "netflix-clone-ec7dc",
+  storageBucket: "netflix-clone-ec7dc.appspot.com",
+  messagingSenderId: "502510293013",
+  appId: "1:502510293013:web:1c505b0d1867aecd423e1c"
+};
