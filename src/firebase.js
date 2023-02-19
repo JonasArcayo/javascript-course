92df5642e0086bf52e84513eef27e884a523585a
Jonas <jonasarcayo@gmail.com>
2023-02-19 00:17:45 -0500

    feat: add Singin form

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAV8Xzrwm85I8gNrIk2UX27q-NS2EcgsP4",
   authDomain: "netflix-clone-ec7dc.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "502510293013",
   appId: "1:502510293013:web:1c505b0d1867aecd423e1c"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
