58f1ecd2c71cda3aefad44120891bb804a5ababf
Jonas <jonasarcayo@gmail.com>
2023-02-19 17:30:40 -0500

    feat: add signin and signup functionalities

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAV8Xzrwm85I8gNrIk2UX27q-NS2EcgsP4",
